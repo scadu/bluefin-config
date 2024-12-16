e546818455cb8ae580ff64aa8d4c5f8acdd52961
Łukasz Jendrysik <scadu9@gmail.com>
2024-12-16 17:55:25 +0100

    force touch

--- build.sh
+++ build.sh
@@ -31,4 +31,4 @@ chmod +x /tmp/1password.sh && \
 ### activate brcmfmac module on boot
 # workaround for broadcom-wl shenanigans in bluefin and aurora
 # https://github.com/ublue-os/bluefin/issues/1783#issuecomment-2546018436
-touch /usr/lib/{NetworkManager/conf.d/90-broadcom-wl.conf,modprobe.d/broadcom-wl-blacklist.conf}
+touch -f /usr/lib/{NetworkManager/conf.d/90-broadcom-wl.conf,modprobe.d/broadcom-wl-blacklist.conf}
